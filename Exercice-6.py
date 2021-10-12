067a677f4b720d19efcfee901ac8aaf84f90fc7b
BananeChocolat <jy.aurelien@gmail.com>
2021-10-12 15:22:18 +0200

    Update Exercice-6.py

--- Exercice-6.py
+++ Exercice-6.py
@@ -5,7 +5,9 @@ class Rectangle:
         self.largeur = largeur
 
     def perimetre(self):
+        """Périmètre : 2*Longueur + 2*Largeur"""
         return self.longueur*2 + self.largeur*2
 
     def aire(self):
+        """Surface : Longueur * Largeur"""
         return self.longueur*self.largeur
